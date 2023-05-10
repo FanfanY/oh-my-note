68111775efe302208f693954c2ea3c778e1f592f
qiuyanxi <qiuyanxi1992@gmail.com>
2023-05-10 17:33:23 +0800

    feat: add welcome text in the page with animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import styles from 'src/app/page.module.scss'
 export default function Home() {
-  return <main>home</main>
+  return (
+    <main className={styles['home-content']}>
+      <h1 className={styles['home-content-text']}>Welcome Here !!!</h1>
+    </main>
+  )
 }
