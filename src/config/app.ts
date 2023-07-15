d2e3d1e204cd903076a665474617a06bbf9c1476
Eva Yang <eva.yang1@booking.com>
2023-07-15 17:38:56 +0800

    feat: update path config

--- src/config/app.ts
+++ src/config/app.ts
@@ -5,6 +5,7 @@ const AppConfig = {
       path: '/docs/English',
     },
     { title: 'General', path: '/docs/General/命令/0' },
+    { title: 'Java', path: '/docs/Java/Database/0' },
   ],
   docsPath: 'docs',
   suffix: '.md',
